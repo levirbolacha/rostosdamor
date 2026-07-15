e8a9aafdb9ffc5c92470711e37ade579cde153be
levirbolacha <levi.bolacha@gmail.com>
2026-07-15 20:34:41 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log(pessoas);
+const galeria = document.getElementById("galeria");
+
+const pessoa = pessoas[0];
+
+galeria.innerHTML = `
+    <img src="imagens/${pessoa.foto}" alt="${pessoa.nome}">
+`;
