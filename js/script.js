3fb021edc22f1afc7369bc70318d621b2c67817f
levirbolacha <levi.bolacha@gmail.com>
2026-07-16 12:20:03 +0100

    Modal funcional

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-alert("SCRIPT CARREGADO");
-
 const galeria = document.getElementById("galeria");
+
+const modal = document.getElementById("modal");
+const modalFoto = document.getElementById("modal-foto");
+const modalNome = document.getElementById("modal-nome");
+const fechar = document.getElementById("fechar");
 
 galeria.innerHTML = "";
 
@@ -8,9 +11,46 @@ for (const pessoa of pessoas) {
 
     galeria.innerHTML += `
         <div class="cartao">
-            <img src="imagens/${pessoa.foto}" alt="${pessoa.nome}">
+
+            <img
+                src="imagens/${pessoa.foto}"
+                alt="${pessoa.nome}"
+                data-foto="${pessoa.foto}"
+                data-nome="${pessoa.nome}">
+
             <h3>${pessoa.nome}</h3>
+
         </div>
     `;
 
 }
+
+const imagens = document.querySelectorAll(".cartao img");
+
+imagens.forEach(imagem => {
+
+    imagem.addEventListener("click", function(){
+
+        modal.style.display = "flex";
+        modalFoto.src = "imagens/" + this.dataset.foto;
+        modalNome.textContent = this.dataset.nome;
+
+    });
+
+});
+
+fechar.addEventListener("click", function(){
+
+    modal.style.display = "none";
+
+});
+
+modal.addEventListener("click", function(event){
+
+    if(event.target === modal){
+
+        modal.style.display = "none";
+
+    }
+
+});
