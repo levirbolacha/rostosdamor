3fe37ee72e8a1f864c3632823076f9ada4528c86
levirbolacha <levi.bolacha@gmail.com>
2026-07-16 11:58:08 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
-const galeria = document.getElementById("galeria");
+alert("SCRIPT CARREGADO");
 
-const modal = document.getElementById("modal");
-const modalFoto = document.getElementById("modal-foto");
-const modalNome = document.getElementById("modal-nome");
-const fechar = document.getElementById("fechar");
+const galeria = document.getElementById("galeria");
 
 galeria.innerHTML = "";
 
@@ -11,27 +8,9 @@ for (const pessoa of pessoas) {
 
     galeria.innerHTML += `
         <div class="cartao">
-            <img
-                src="imagens/${pessoa.foto}"
-                alt="${pessoa.nome}"
-                data-foto="${pessoa.foto}"
-                data-nome="${pessoa.nome}">
+            <img src="imagens/${pessoa.foto}" alt="${pessoa.nome}">
             <h3>${pessoa.nome}</h3>
         </div>
     `;
 
 }
-
-const imagens = document.querySelectorAll(".cartao img");
-
-imagens.forEach(imagem => {
-
-    imagem.addEventListener("click", function(){
-
-        console.log(modal);
-        console.log(modalFoto);
-        console.log(modalNome);
-
-    });
-
-});
